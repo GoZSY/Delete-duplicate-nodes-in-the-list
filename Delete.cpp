e6c94bddae764668f9657fb6f2e090355d77b325
GoZSY <13207508051@163.com>
2016-04-26 00:27:43 +0800

    Update Delete.cpp

--- Delete.cpp
+++ Delete.cpp
@@ -32,3 +32,39 @@ public:
         return pHead;
     }
 };
+///使用三个滑动指针和一个初始节点
+class Solution {
+public:
+    ListNode* deleteDuplication(ListNode* pHead)
+    {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = pHead;
+        ListNode *p1 = dummy;
+        ListNode *p2 = pHead;
+        ListNode *p3 = p2 != NULL ? p2->next : NULL;   
+        while(p3){  
+            bool issame = false;
+            while(p3 && p3->val == p2->val){
+                issame = true;
+                p3 = p3->next;
+            }
+            if(issame){
+                ListNode* p = p2;
+                while(p != p3){
+                    ListNode *tmp = p->next;
+                    delete p;
+                    p = tmp;
+                }
+                p1->next = p3;
+                p2 = p3;
+                p3 = p2 != NULL ? p2->next : NULL;   
+            }
+            else{
+                p1 = p1->next;
+                p2 = p2->next;
+                p3 = p3 != NULL ? p3->next : NULL;
+            }           
+        }
+        return dummy->next;
+    }
+};
